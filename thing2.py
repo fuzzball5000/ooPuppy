e4204acb1fa014660df482cde8084db5a7cc7a83
Gary <gary.richardson@capitalone.com>
2017-08-11 16:03:42 +0100

    spaces

--- thing2.py
+++ thing2.py
@@ -3,33 +3,34 @@ import sys
 class PupThing():
 
     def __init__(self):
-        #Whatever goes up here is availbale to the entire class
-        self.pupdict = {"toy":sys.argv[1],"bark":"borf","tail":"unknown"}
+        # Whatever goes up here is availbale to the entire class
+        self.pupdict = {"toy": sys.argv[1], "bark": "borf", "tail": "unknown"}
 
     def loop_me(self,somevalue):
         if self.pupdict["toy"] == somevalue:
-            #Self refers to any variable, function, object within the same class
+            # Self refers to any variable, function, object within the
+            # same class
             status = True
         else:
             status = False
-        #Return lets the function emit something back to the function which called
-        #it
+        # Return lets the function emit something back to the function which
+        # called it
         return status
 
     def run(self):
         if self.loop_me("bone") and self.pupdict["bark"] == "borf":
-            #self.loop_me is a function which returns a True|False status based
-            #on the logic contained within it. It accepts a parameter, which it
-            #sets as teh variable 'somevalue'
+            # self.loop_me is a function which returns a True|False status
+            # based on the logic contained within it.
+            # It accepts a parameter, which it sets as teh variable 'somevalue'
 
-            #Test the dictionary, then update it!
+            # Test the dictionary, then update it!
             self.pupdict["tail"] = "wag"
         else:
-            #Pup wants a bone, not a squeaky
+            # Pup wants a bone, not a squeaky
             self.pupdict["tail"] = "nowag"
-        #return the updated pupdict to the print function which called it.
+        # return the updated pupdict to the print function which called it.
         return self.pupdict
 
 if __name__ == "__main__":
-    #If the program is being run as itself, do this:
+    # If the program is being run as itself, do this:
     print PupThing().run()
